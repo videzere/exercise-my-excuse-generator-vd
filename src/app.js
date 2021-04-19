095a92b26ae9c4ccf2b2a6bb2a6eaae50c6b8ae7
Victoria Dezerega <videzere@gmail.com>
2021-04-18 20:22:35 -0400

    Actualizar GitHud v2

--- src/app.js
+++ src/app.js
@@ -2,10 +2,52 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //console.log("Hello from the console!");
+  // let excuse = "My cat ate the homework";
+  // document.querySelector("#excuse").innerHTML = excuse;
+
+  let who = [
+    "My cat",
+    "My parrot",
+    "Their mom",
+    "The termites",
+    "My cousin",
+    "The neighbor"
+  ];
+  let action = [
+    "ate",
+    "painted",
+    "smashed",
+    "hid",
+    "broken",
+    "burnt",
+    "cut",
+    "shook"
+  ];
+  let what = [
+    "the glasses",
+    "the pills",
+    "the boat",
+    "the cookies",
+    "the soda",
+    "the book"
+  ];
+  let when = [
+    "after the flight",
+    "when the war ended",
+    "before the game started",
+    "during the conference",
+    "before drinking it",
+    "during the class"
+  ];
+
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomAction = Math.floor(Math.random() * action.length);
+  let randomWhat = Math.floor(Math.random() * what.length);
+  let randomWhen = Math.floor(Math.random() * when.length);
+
+  document.querySelector("#excuse").innerHTML = `${who[randomWho]} 
+    ${action[randomAction]} ${what[randomWhat]} ${when[randomWhen]}`;
 };
